008998b38a8139867c2f4e482ea5b1fca4676e2a
Maligno <guillermoivansalgadosandoval@gmail.com>
2024-08-13 17:09:55 -0500

    New routes for front add

--- server/server.js
+++ server/server.js
@@ -13,6 +13,42 @@ app.get('/', (req, res) => {
     res.send("What up bro")
 })
 
+app.get( "/register", (req, res) => {
+    res.send( "register");
+});
+
+app.get ("/home", (req, res) => {
+    res.send("home");
+});
+
+app.get ("/connect", (req, res) => {
+    res.send( "connect");
+});
+
+app.get ("/logout", ( req, res) => {
+    res.send( "logout");
+});
+
+app.get ("/contact", (req, res)  => {
+    res.send("contact");
+});
+
+app.get ("/user",( req,res) => {
+    res.send("user");
+});
+
+app.get ("/profile",( req, res) => {
+    res.send("profile");
+});
+
+app.get ("/login",( req, res) => {
+    res.send("login");
+});
+
+app.get ("/unconnect",( req,res) => {
+    res.send( "unconnect");
+});
+
 app.use((req, res) => {
     res.status(404).send("Not Found")
 })
